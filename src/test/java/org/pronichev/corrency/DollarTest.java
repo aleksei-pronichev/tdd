8753352914cf742fdcad77ca83c95491947b998f
Aleksei_Pronichev <aleksei.pronichev@outlook.com>
2024-03-10 23:04:31 +0500

    chapter 2

--- src/test/java/org/pronichev/corrency/DollarTest.java
+++ src/test/java/org/pronichev/corrency/DollarTest.java
@@ -1,15 +1,22 @@
 package org.pronichev.corrency;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DollarTest {
 
-    @Test
-    void testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+    @ParameterizedTest
+    @CsvSource({
+        "2, 10",
+        "3, 15",
+        "4, 20"
+    })
+    void testMultiplication(int multiplier, int expected) {
+        var five = new Dollar(5);
+        var product = five.times(multiplier);
+        five.times(multiplier);
+        assertEquals(expected, product.amount);
     }
 }
